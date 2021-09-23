513fc8687cd422aa15e92df0eb9909beffb13506
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-09-23 16:50:51 +0530

    Add images and card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      username: 'nature',
+      content: 'I saw this neat tree today',
+      imageUrl: '../assets/tree.jpeg',
+    },
+    {
+      title: 'Sonwy Mountain',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain',
+      imageUrl: '../assets/mountain.jpeg',
+    },
+    {
+      title: 'Mountain Biking',
+      username: 'biking1222',
+      content: 'I did some biking today',
+      imageUrl: '../assets/biking.jpeg',
+    },
+  ];
 }
